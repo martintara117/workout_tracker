2d4059493dcded58dec97f0fb8aa7c336d97433b
Tara Martin <martin.tara117@gmail.com>
2021-01-23 15:08:57 -0500

    added models folder with index.js and workout.js. required mongoose, scheme and created a workoutschema.

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -6,11 +6,32 @@ router.get("/", (req, res) => {
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/api/workouts", (req, res) => {
+  // get last workout
+  // needs to return an array of workouts
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  // adds exercise to a previously made workout
+  // needs to return an array of workouts
+  // id is the id of the workout
+});
+
+router.post("/api/workouts", (req, res) => {
+  // creates new workout
+  // assigns an id
+  // returns a workout object without any exercises in it
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  // unknown function
 });
 
 module.exports = router;
